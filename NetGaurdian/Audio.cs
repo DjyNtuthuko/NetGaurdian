2724f7b1e10acb42831a8ace1ce3f20aa32152ef
Mike Ndaba <Mike Ndaba@MIKE>
2026-04-04 00:06:08 +0200

    Add project files.

---
+++ NetGaurdian/Audio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace NetGuardian
+{
+    internal class Audio
+    {
+        public void Playaudio()
+        {
+            // Get project directory
+            string projectPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Remove bin/debug folder
+            string newPath = projectPath.Replace("bin\\Debug\\", "");
+
+            // Combine path with audio file
+            string audioPath = Path.Combine(newPath, "assets", "WelcomeAudio.wav");
+
+            try
+            {
+                SoundPlayer player = new SoundPlayer(audioPath);
+                player.PlaySync();
+            }
+            catch
+            {
+                Console.WriteLine("Audio could not play.");
+            }
+        }
+    }
+}
